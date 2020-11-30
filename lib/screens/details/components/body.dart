352c08410986f46302a9fa1295dc3d16983b01ff
Utsav Kuinkel <utshaav583@gmail.com>
2020-11-30 09:02:01 +0545

    Addep price and picture in the details page

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -17,12 +17,29 @@ class Body extends StatelessWidget {
             child: Stack(
               children: [
                 Container(
-                  margin: EdgeInsets.only(top: size.height * 0.3),
-                  height: 500,
+                  margin: EdgeInsets.only(top: size.height * 0.35),
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.12,
+                    left: kDefaultPadding,
+                    right: kDefaultPadding,
+                  ),
+                  // height: 500,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius:
                         BorderRadius.vertical(top: Radius.circular(20)),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "Color",
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
                 Padding(
@@ -43,6 +60,38 @@ class Body extends StatelessWidget {
                               color: Colors.white,
                               fontWeight: FontWeight.bold,
                             ),
+                      ),
+                      SizedBox(
+                        height: kDefaultPadding,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(text: "Price\n"),
+                                TextSpan(
+                                  text: "\$${product.price}",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline4
+                                      .copyWith(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            width: kDefaultPadding,
+                          ),
+                          Expanded(
+                            child: Image.asset(
+                              product.image,
+                              fit: BoxFit.fill,
+                            ),
+                          )
+                        ],
                       ),
                     ],
                   ),
